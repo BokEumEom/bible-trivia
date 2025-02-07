2db9f01ca22b8fc1d0cc93493ba644e5991ee11c
bokeum <bokmail83@gmail.com>
2025-02-07 10:59:39 +0900

    ui update

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,11 +1,12 @@
 // src/components/Header.jsx
-import styles from "../styles/header.module.css"; // 별도 CSS 파일로 분리(예: header.module.css)
+import styles from "../styles/header.module.css";
 
 const Header = () => {
   return (
     <header className={styles.navBar}>
-      {/* 필요에 따라 로고나 아이콘 추가 */}
-      <h1 className={styles.navTitle}>Bible Trivia</h1>
+      <div className={styles.banner}>
+        <h1 className={styles.navTitle}>Bible Trivia</h1>
+      </div>
     </header>
   );
 };
